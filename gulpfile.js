dd0bd080f04a634246e9146b0aa6d2718b97ef30
hajpson <kacpah@hotmail.com>
2020-03-25 21:42:25 +0100

    Exercise
    Whole exercise done

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('js', function() {
-  return gulp.src('src/js/*.js')
+  return gulp.src('src/js/index.js')
     .pipe(babel({
       presets: ['@babel/env']
     }))
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 /* Here goes your JS code */
+function isEmpty() {
+    let x = document.forms["theForm"]["pswd"].value;
+    let y = document.forms["theForm"]["mail"].value;
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let check = document.getElementById("checkbox");
+    if ((x == "") || (!(y.match(mailformat))) || (check.checked == false)) {
+        alert("Invalid info or unchecked checkbox!")
+        return false;
+    }
+    else {
+        document.getElementsByClassName("popup")[0].style.display = "none";
+        document.getElementById("hide-popup-form").style.display = "block";
+        document.getElementById("show-popup-form").style.display = "none";
+        event.preventDefault()
+    }
+}
+
+function openThis() {
+    document.getElementsByClassName("popup")[0].style.display = "flex";
+    document.getElementById("show-popup-form").style.display = "none";
+}
+
+function closeThis() {
+    document.getElementsByClassName("popup")[0].style.display = "none";
+    document.getElementById("show-popup-form").style.display = "block";
+}
